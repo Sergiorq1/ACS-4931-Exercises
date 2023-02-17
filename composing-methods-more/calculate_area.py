39f28a4d00205e93926922b90a90c6b41399ec78
Sergiorq1 <sergiorq12016@gmail.com>
2023-02-17 10:08:48 -0700

    finished refactoring 7-10

--- composing-methods-more/calculate_area.py
+++ composing-methods-more/calculate_area.py
@@ -2,14 +2,14 @@
 # Rename Method
 # Reference: https://parade.com/1039985/marynliles/pick-up-lines/
 
-def cal_un_gr(graph):   # TODO: Rename this function to reflect what it's doing.
+def cal_area_under_graph(graph):   # TODO: Rename this function to reflect what it's doing.
     """Calculate the area under the input graph."""
     # bla bla bla.
     pass
 
 #####################
 
-def get_value(li):  # TODO: Rename this function to reflect what it's doing.
+def get_greatest_value(li):  # TODO: Rename this function to reflect what it's doing.
     m = li[0]
     for value in li:
         if value > m:
@@ -18,10 +18,10 @@ def get_value(li):  # TODO: Rename this function to reflect what it's doing.
 
 
 li = [5, -1, 43, 32, 87, -100]
-print(get_value(li))
+print(get_greatest_value(li))
 
 ############################
-def process(sentence):  # TODO: Rename this function to reflect what it's doing.
+def create_list_of_words(sentence):  # TODO: Rename this function to reflect what it's doing.
     words = sentence[0:].split(' ')
     return words
 
--- composing-methods-more/kinetic_energy.py
+++ composing-methods-more/kinetic_energy.py
@@ -5,34 +5,39 @@ class Distance:
     def __init__(self, value, unit):
         self.unit = unit
         self.value = value
+    def find_speed(self, time):
+        if self.unit != 'km':
+            if self.unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
+                # convert from light-year to km unit
+                in_km = self.value * 9.461e12
+                distance = Distance(in_km, "km")
+            else:
+                print ("unit is Unknown")
+                return
+        speed = distance.value/time # [km per sec]
+        return speed
 
 class Mass:
     def __init__(self, value, unit):
         self.value = value
         self.unit = unit
-
-def calculate_kinetic_energy(mass, distance, time):
-    if distance.unit != 'km':
-        if distance.unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
-            # convert from light-year to km unit
-            in_km = distance.value * 9.461e12
-            distance = Distance(in_km, "km")
+    def find_mass(self):
+        if self.unit != 'kg':
+            if self.unit == "solar-mass":
+                # convert from solar mass to kg
+                value = self.value * 1.98892e30 # [kg]
+                mass = Mass(value, 'kg')
         else:
             print ("unit is Unknown")
             return
-    speed = distance.value/time # [km per sec]
-    if mass.unit != 'kg':
-        if mass.unit == "solar-mass":
-            # convert from solar mass to kg
-            value = mass.value * 1.98892e30 # [kg]
-            mass = Mass(value, 'kg')
-        else:
-            print ("unit is Unknown")
-            return
+        return mass.value
 
-    kinetic_energy = 0.5 * mass.value * speed ** 2
+def calculate_kinetic_energy(found_mass, found_speed):
+    kinetic_energy = 0.5 * found_mass * found_speed ** 2
     return kinetic_energy
 
 mass = Mass(2, "solar-mass")
+found_mass = mass.find_mass()
 distance = Distance(2, 'ly')
-print(calculate_kinetic_energy(mass, distance, 3600e20))
+found_speed = distance.find_speed(3600e20)
+print(calculate_kinetic_energy(found_mass, found_speed))
