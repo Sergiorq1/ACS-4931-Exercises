8ef001cc0247a1a022068e5adfbbefdc93ea663f
Sergiorq1 <sergiorq12016@gmail.com>
2023-02-21 11:36:28 -0700

    refactored code

--- other-refactor-techniques/actors.py
+++ other-refactor-techniques/actors.py
@@ -7,14 +7,27 @@ movies = [['Tenet', 'Vita & Virgina', 'Guardians of the Galexy', 'The Great Gats
      ['Ace Ventura', 'The Mask', 'Dubm and Dumber', 'The Truman Show', 'Yes Man']]
 emails = ['deb@makeschool.com', 'jim@makeschool.com']
 
-def send_hiring_email(email):
-    print("email sent to: ", email)
-    
-for i, value in enumerate(emails):
-    if birth_year[i] > 1985:
-        print(first_names[i], last_names[i])
+class Person:
+    def __init__(self, first_name, last_name, birth_year, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
+        self.email = email
+    def send_hiring_email(self):
+        print("email sent to: ", self.email)
+    def print_info_after_1985(self):
+        if self.birth_year < 1985:
+            return 
+        print(self.first_name, self.last_name)
         print('Movies Played: ', end='')
         for m in movies[i]:
             print(m, end=', ')
-        print()
-        send_hiring_email(value)
+    
+for i in range(len(first_names)):
+    new_person = Person(first_names[i], last_names[i], birth_year[i], emails[i])
+    new_person.print_info_after_1985()
+    new_person.send_hiring_email()
+
+
+    
+
--- other-refactor-techniques/draw_shapes.py
+++ other-refactor-techniques/draw_shapes.py
@@ -1,58 +1,44 @@
 # by Kami Bigdely
 # Extract superclass.
-class Circle:
-    
-    def __init__(self, x, y, r, visible = True):
-      self.center_x = x
-      self.center_y = y
-      self.r = r
-      self.visible = visible
-      
-    def display(self):
-        print('drew the circle.')
-        
-    def set_visible(self,is_visible):
-        self.visible = is_visible
-        
-    def get_center(self):
-        return self.center_x, self.center_y
-    
-    
-class Rectangle:
-    
-    def __init__(self, x, y, width, height, visible = True):
-        # left-bottom corner.
+class Shape:
+    def __init__(self, shape, x, y, width, visible=True, height=0):
+        self.shape = shape
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.visible = visible
-        
+    
     def display(self):
-        if self.visible:
-            print('drew the rectable.')
-            
+        if not self.visible:
+            return -1
+        if self.shape == 'rect':
+            print('drew the rectangle')
+        elif self.shape == 'circle':
+            print('drew the circle')
+
     def hide(self):
         self.visible = False
-        
-    def make_visible(self):
+
+    def set_visible(self):
         self.visible = True
-        
+    
     def get_center(self):
-        return self.x + self.width/2, \
+        if self.shape == 'rect':
+            return self.x + self.width/2, \
                self.y + self.height/2 
-
-
+        elif self.shape == 'circle':
+            return self.x, self.y
 
 if __name__ == "__main__":
-    circle = Circle(0,0,10, False)
-    circle.set_visible(True)
+    circle = Shape('circle',0,0,10, False)
+    circle.set_visible()
     circle.display()
     print('center point',circle.get_center())
 
-    rect = Rectangle(10, 10, 20, 5)
+    rect = Shape('rect',10, 10, 20, True, 5)
     rect.hide()
     rect.display() # does not display because it's hidden.
-    rect.make_visible()
+    rect.set_visible()
     rect.display()
     print('center point',rect.get_center())
